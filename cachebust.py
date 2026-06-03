a19f3674746c57d026553d03d28b4c6544d6fd72
Charles Kim <charleskimisr@gmail.com>
2026-06-04 08:52:02 +0900

    Bust browser cache to v2.11 to propagate Phobos removal

--- cachebust.py
+++ cachebust.py
@@ -5,7 +5,7 @@ html_files = glob.glob('*.html')
 for file in html_files:
     with open(file, 'r', encoding='utf-8') as f:
         content = f.read()
-    if 'v=2.00' in content:
-        content = content.replace('v=2.00', 'v=2.10')
+    if 'v=2.10' in content:
+        content = content.replace('v=2.10', 'v=2.11')
         with open(file, 'w', encoding='utf-8') as f:
             f.write(content)
